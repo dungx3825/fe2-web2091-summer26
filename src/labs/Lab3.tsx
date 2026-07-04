e81b7333fecdc78520ecfc9b40bf555cf39a6db1
vanhoa690 <vanhoa690@gmail.com>
2026-07-04 16:45:40 +0700

    lab3

---
+++ src/labs/Lab3.tsx
@@ -0,0 +1,13 @@
+import { Form, Input, Button } from "antd";
+
+function Lab3() {
+  return (
+    <Form>
+      <Form.Item>
+        <Input />
+      </Form.Item>
+      <Button>Submit</Button>
+    </Form>
+  );
+}
+export default Lab3;
