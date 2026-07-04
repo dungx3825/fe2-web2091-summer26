330ecf2a0fac874c56b13213d56a2b147094d5a4
vanhoa690 <vanhoa690@gmail.com>
2026-07-04 16:58:43 +0700

    update

--- src/labs/Lab3.tsx
+++ src/labs/Lab3.tsx
@@ -1,12 +1,20 @@
-import { Form, Input, Button } from "antd";
+import { Form, Input, Button, InputNumber } from "antd";
 
 function Lab3() {
+  const onFinish = (data: any) => {
+    console.log(data);
+  };
   return (
-    <Form>
-      <Form.Item>
+    <Form onFinish={onFinish}>
+      <Form.Item label="Title" name="title">
         <Input />
       </Form.Item>
-      <Button>Submit</Button>
+      <Form.Item label="Price" name="price">
+        <InputNumber />
+        {/* Select */}
+        {/* Checkbox */}
+      </Form.Item>
+      <Button htmlType="submit">Submit</Button>
     </Form>
   );
 }
